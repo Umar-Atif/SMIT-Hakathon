c8adce85a161340b9a5f9fc57680f7e0af420f88
Umar-Atif <umaratifweb@gmail.com>
2025-10-21 23:20:55 +0500

    Serverless function created

--- Server/index.js
+++ Server/index.js
@@ -11,17 +11,28 @@ const express = require('express');
 const app = express();
 
 //Database
-mongoose.connect(process.env.MONGO_URI, { dbName: "HealthMate" }) 
-    .then(() => {
-        console.log('DB Connected');
-    })
-    .catch((err) => {
-        console.error(err)
-    });
+let isConnected = false;
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { dbName: "Ecommerce" });
+        isConnected = true;
+        console.log("✅ MongoDB connected");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+}
+app.use((req, res, next) => {
+    if (!isConnected) connectDB();
+    next();
+});
 
 // Middlewares
+const allowedOrigins = ["http://localhost:5173"];
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+        else callback(new Error("Not allowed by CORS"));
+    },
     credentials: true
 }));
 app.use(express.json());
@@ -37,6 +48,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/members", memberRoutes);
 app.use("/api/reports", reportRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running")
-});
+// app.listen(process.env.PORT, () => {
+//     console.log("Server is running")
+// });
+module.exports = app;
